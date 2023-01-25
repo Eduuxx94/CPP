602b0e7fd6719cbdde6af1d219843205b6edb91f
Eduuxx94 <eduuxx94@gmail.com>
2023-01-25 21:30:12 +0100

    done mod01 ex04-fix bugs

--- Module_01/ex04/main.cpp
+++ Module_01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ede-alme <ede-alme@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/24 22:33:41 by ede-alme          #+#    #+#             */
-/*   Updated: 2023/01/25 12:47:07 by ede-alme         ###   ########.fr       */
+/*   Updated: 2023/01/25 21:28:07 by ede-alme         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,30 +14,32 @@
 #include <fstream>
 
 int main(int argc, char** argv){
-    if (argc == 4) {
-        std::fstream	file;
-        file.open(argv[1], std::ios::in);
-		if (!file.is_open()){
-			std::cout << "File not find!" << std::endl;
-			return 1;
+    if (argc != 4) {
+		std::cout << "Please use:" << std::endl << "./replace <filename>   \"str to search\"   \"str to replace\"" << std::endl;
+		return 1;
+	}
+	if (std::string(argv[2]) == std::string(argv[3])){
+		std::cout << "Error! The args are equal..." << std::endl;
+		return 1;
+	}
+	std::fstream	file;
+	file.open(argv[1], std::ios::in);
+	if (!file.is_open()){
+		std::cout << "File not find!" << std::endl;
+		return 1;
+	}
+	std::ofstream	outfile((std::string(argv[1]) + std::string(".replace")).c_str(), std::ios::out);
+	std::string	txt;
+	std::getline(file, txt, '\0');
+	int	count = 0;
+	for (int i = 0; txt[i + count]; i++){
+		if ((long unsigned int)(count + i) == txt.find(argv[2], i + count)){
+			outfile << std::string(argv[3]);
+			count += (std::string(argv[2]).length() - 1);
 		}
-		std::ofstream	outfile((std::string(argv[1]) + std::string(".replace")).c_str(), std::ios::out);
-		std::cout << "\033[0;32mStarting copy file.....\033[0m" << std::endl;
-		std::string	line;
-		for (int i = 1; std::getline(file, line); i++){
-			if (line == std::string(argv[2])){
-				outfile << std::string(argv[3]) << std::endl;
-				std::cout << "Replaced ";
-			}
-			else{
-				outfile << line << std::endl;
-			}
-			std::cout << "Line " << i << " was insert!" << std::endl;
+		else{
+			outfile << txt[i + count];
 		}
-		std::cout << "Output is: " << std::string(argv[1]) + ".replace" << std::endl;
-		file.close();
-		outfile.close();
-    }
-    else
-        std::cout << "Please use:" << std::endl << "./replace <filename>   \"str to look\"   \"str to replace\"" << std::endl;
+	}
+	return 0;
 }
